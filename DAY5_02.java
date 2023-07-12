01016247129005f6cfb0c2237cc4295001d648bc
ZJJUP <139037120+ZJJUP@users.noreply.github.com>
2023-07-12 16:23:48 +0800

    Update DAY5_02.java

--- DAY5_02.java
+++ DAY5_02.java
@@ -1,12 +1,12 @@
-public class DAY5_02 {
-    public static void main(String[] args) {
+公共 class DAY5_02 {
+    公共 static void main(String[] args) {
 //数组元素对调
         int[] a ={1,4,8,5,7,9};
         for (int i = 0 ,  j = 5 ; i<=j ; i++ ) {
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            System.out.println(a[i]);
+                int temp = a[i];                       第一次这里写错了   int temp = a[i]
+                a[i] = a[j];                                              a[j] = temp
+                a[j] = temp;                                               a[i] = a[j]
+            System.out.println(a[i]);                        最后输出的是148577
         }
     }
 }
